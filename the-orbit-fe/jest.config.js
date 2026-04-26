82b62906b206f8acf301872b1cc007c3c012fda4
Axololt7923 <lqkiet7923@gmail.com>
2026-04-26 23:32:54 +0700

    fix coverage page

--- the-orbit-fe/jest.config.js
+++ the-orbit-fe/jest.config.js
@@ -3,15 +3,9 @@ module.exports = {
   setupFilesAfterEnv: ['./jest.setup.js'],
   collectCoverage: true,
   collectCoverageFrom: [
-    'app/(tabs)/index.tsx', 
-    
-    // '!app/_layout.tsx',
-    // '!app/modal.tsx',
-    // '!app/(tabs)/_layout.tsx',
-    // '!app/(tabs)/explore.tsx',
-    // '!components/**', // Nếu bạn không test components thì loại bỏ cả folder này
-    '!**/node_modules/**',
-  ],
+    "app/**/*.{ts,tsx}",
+    "!**/node_modules/**"
+],
   coverageReporters: ['lcov', 'text', 'html'],
   coverageDirectory: 'coverage',
   // ... các cấu hình khác
